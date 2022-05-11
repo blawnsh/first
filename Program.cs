43f6f7aa0ac4c7ff0851691c27f50fae5884284b
blawnsh <salmansh@code.edu.az>
2022-05-11 21:31:49 +0400

    Created Program.cs

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace first
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type Your Name");
+            string name = Console.ReadLine();
+            bool isIncludeA = false;
+
+            for (int i = 0; i < name.Length; i++)
+            {
+                char a = name[i];
+                if(a == 'a')
+                {
+                    isIncludeA = true;
+                }
+                
+            }
+
+            Console.WriteLine(isIncludeA);
+        }
+    }
+}
